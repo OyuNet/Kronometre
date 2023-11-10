d43e7caee2a87385a81c9dc4391d1b77f48d5613
OyuNet <oyunarda662@gmail.com>
2023-11-10 21:10:44 +0300

    bitti.

---
+++ src/app/page.jsx
@@ -0,0 +1,80 @@
+"use client"
+
+import React, { useEffect } from 'react'
+import './style.css'
+
+export default function Home() {
+  const [second, setSecond] = React.useState(0)
+  const [minute, setMinute] = React.useState(0)
+  const [hour, setHour] = React.useState(0)
+  const [start, setStart] = React.useState(false)
+  const [pSecond, setpSecond] = React.useState("00")
+  const [pMinute, setpMinute] = React.useState("00")
+  const [pHour, setpHour] = React.useState("00")
+  const [times, setTimes] = React.useState([])
+
+  useEffect(() => {
+    let interval;
+
+    if (start) {
+      interval = setInterval(() => {
+        setSecond((prevSecond) => {
+          if (prevSecond >= 59) {
+            setMinute((prevMinute) => {
+              if (prevMinute >= 59) {
+                setHour((prevHour) => prevHour + 1);
+                return 0;
+              }
+              return prevMinute + 1;
+            });
+            return 0;
+          }
+          return prevSecond + 1;
+        });
+      }, 1000);
+    }
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [start]);
+
+  useEffect(() => {
+    setpHour(hour > 9 ? String(hour) : `0${hour}`);
+    setpMinute(minute > 9 ? String(minute) : `0${minute}`);
+    setpSecond(second > 9 ? String(second) : `0${second}`);
+  }, [second, minute, hour]);
+
+  return (
+    <main>
+      <h1 className='text-center m-2 title'>Kronometre</h1>
+      <div className='flex'>
+        <div className='grow'></div>
+        <div className='flex grow-0 gap-2 m-3 justify-center outline-dashed timer-container'>
+          <h2>{pHour}</h2>
+          <h2>:</h2>
+          <h2>{pMinute}</h2>
+          <h2>:</h2>
+          <h2>{pSecond}</h2>
+        </div>
+        <div className='grow'></div>
+      </div>
+      <div className='flex gap-4 m-4 justify-center'>
+        <button className="button-18" onClick={() => {setStart(true)}}>Başlat</button>
+        <button className="button-18" onClick={() => {setStart(false)}}>Durdur</button>
+        <button className='button-18' onClick={() => {
+          const currentTime = `${hour}:${minute}:${second}`
+          setTimes([...times, currentTime])
+        }}>Zamanı Kaydet</button>
+      </div>
+      <div className='custom-width gap-1 m-2'>
+        {times.map((x, index) => (
+          <div key={index} className="time-record">
+            <span className="index">{index + 1})</span>
+            <span className="time">{x}</span>
+          </div>
+        ))}
+      </div>
+    </main>
+  )
+}
